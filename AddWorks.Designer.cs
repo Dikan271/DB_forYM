2f8e129e7b510370676f2fdfd41529e8905fc405
Dikan271 <48617366+Dikan271@users.noreply.github.com>
2020-02-21 15:51:24 +0200

    Add files via upload

---
+++ AddWorks.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace YM_3._0
+{
+    partial class AddWorks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleField = new System.Windows.Forms.TextBox();
+            this.listUsers = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonCencel = new System.Windows.Forms.Button();
+            this.buttonNewUser = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.linkField);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.titleField);
+            this.panel1.Location = new System.Drawing.Point(168, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 101);
+            this.panel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PostLink";
+            // 
+            // linkField
+            // 
+            this.linkField.Location = new System.Drawing.Point(87, 64);
+            this.linkField.Name = "linkField";
+            this.linkField.Size = new System.Drawing.Size(264, 22);
+            this.linkField.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Titile\r\n";
+            // 
+            // titleField
+            // 
+            this.titleField.Location = new System.Drawing.Point(87, 19);
+            this.titleField.Name = "titleField";
+            this.titleField.Size = new System.Drawing.Size(264, 22);
+            this.titleField.TabIndex = 0;
+            // 
+            // listUsers
+            // 
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.ItemHeight = 16;
+            this.listUsers.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.listUsers.Location = new System.Drawing.Point(10, 30);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(152, 180);
+            this.listUsers.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Users";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(87, 3);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 58);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "Add\r\npost";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonCencel
+            // 
+            this.buttonCencel.Location = new System.Drawing.Point(87, 3);
+            this.buttonCencel.Name = "buttonCencel";
+            this.buttonCencel.Size = new System.Drawing.Size(75, 58);
+            this.buttonCencel.TabIndex = 7;
+            this.buttonCencel.Text = "Close";
+            this.buttonCencel.UseVisualStyleBackColor = true;
+            this.buttonCencel.Click += new System.EventHandler(this.buttonCencel_Click);
+            // 
+            // buttonNewUser
+            // 
+            this.buttonNewUser.Location = new System.Drawing.Point(6, 3);
+            this.buttonNewUser.Name = "buttonNewUser";
+            this.buttonNewUser.Size = new System.Drawing.Size(75, 58);
+            this.buttonNewUser.TabIndex = 6;
+            this.buttonNewUser.Text = "New User";
+            this.buttonNewUser.UseVisualStyleBackColor = true;
+            this.buttonNewUser.Click += new System.EventHandler(this.buttonNewUser_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttonNewUser);
+            this.panel2.Controls.Add(this.buttonAdd);
+            this.panel2.Location = new System.Drawing.Point(359, 137);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(171, 68);
+            this.panel2.TabIndex = 10;
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(6, 3);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(75, 58);
+            this.buttonView.TabIndex = 11;
+            this.buttonView.Text = "View all Posts";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.buttonView);
+            this.panel3.Controls.Add(this.buttonCencel);
+            this.panel3.Location = new System.Drawing.Point(168, 137);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(171, 68);
+            this.panel3.TabIndex = 12;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // AddWorks
+            // 
+            this.AcceptButton = this.buttonAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonView;
+            this.ClientSize = new System.Drawing.Size(537, 214);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listUsers);
+            this.Controls.Add(this.panel1);
+            this.Name = "AddWorks";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddWorks";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox linkField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleField;
+        private System.Windows.Forms.ListBox listUsers;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonCencel;
+        private System.Windows.Forms.Button buttonNewUser;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonView;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
---
+++ AddWorks.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace YM_3._0
+{
+    public partial class AddWorks : Form
+    {
+        public AddWorks()
+        {
+            InitializeComponent();
+            listUsers.Items.Clear();
+            Fill_ListBox();
+            listUsers.Show();
+        }
+
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=YM;Integrated Security=True";
+
+        public AddWorks(int selectIndex)
+        {
+            InitializeComponent();
+            this.listUsers.Items.Clear();
+            this.Fill_ListBox();
+            this.listUsers.Show();
+            if(this.listUsers.Items.Count >= selectIndex)
+                this.listUsers.SetSelected(selectIndex, true);
+        }
+
+        private void Fill_ListBox()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("select * from [User]", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                        this.listUsers.Items.Add(reader.GetString(1));
+                }
+                else
+                    this.listUsers.Items.Add("No users");
+            }
+        }
+
+        private void buttonCencel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonNewUser_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegistUserForm regist = new RegistUserForm();
+            regist.Show();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string link = this.linkField.Text;
+            string title = this.titleField.Text;
+            if (title == "")
+                title = "noname";
+            if (!IsValidNote())
+                return;
+            else
+            {
+                SetInDB(title, link);
+                SetInLib();
+            }
+            this.titleField.Clear();
+            this.linkField.Clear();
+        }
+
+        private Boolean IsValidNote()
+        {
+            return (this.linkField.Text != "" && this.listUsers.SelectedIndex != -1);
+        }
+
+        private void SetInDB(string title, string link)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("newPost", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@title",
+                    Value = title
+                };
+                command.Parameters.Add(nameParam);
+                SqlParameter linkParam = new SqlParameter
+                {
+                    ParameterName = "@link",
+                    Value = link
+                };
+                command.Parameters.Add(linkParam);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void SetInLib()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("setinlib", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@uname",
+                    Value = this.listUsers.SelectedItem.ToString()
+                };
+                command.Parameters.Add(nameParam);
+                SqlParameter linkParam = new SqlParameter
+                {
+                    ParameterName = "@urlpost",
+                    Value = this.linkField.Text
+                };
+                command.Parameters.Add(linkParam);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Overview overview = new Overview();
+            overview.Show();
+        }
+    }
+}
